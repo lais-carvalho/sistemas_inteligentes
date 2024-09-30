e2d627998ff8372436bfa6643853e1bbb2bea715
lais-carvalho <95021199+lais-carvalho@users.noreply.github.com>
2024-09-30 10:17:49 +0100

    Mensagem descrevendo suas alterações

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='si',
+    name='sistemas_inteligentes',
     version='0.0.1',
     python_requires='>=3.7',
     package_dir={'': 'src'},
